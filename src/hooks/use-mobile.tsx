9733c84b6c2ab3d2a52639f0340e8ab76e4c7656
Pardi Pardi <pardi.jhr@gmail.com>
2025-11-30 10:26:02 +0000

    saat user menggunakan hp aplikasi muncul error : aplication error : a cl

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -1,19 +1,25 @@
+"use client";
+
 import * as React from "react"
 
 const MOBILE_BREAKPOINT = 768
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
+  const [isMobile, setIsMobile] = React.useState(false)
 
   React.useEffect(() => {
+    const checkIsMobile = () => {
+      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+    };
+
+    // Initial check
+    checkIsMobile();
+
     const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-    const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    }
-    mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    return () => mql.removeEventListener("change", onChange)
+    mql.addEventListener("change", checkIsMobile)
+    
+    return () => mql.removeEventListener("change", checkIsMobile)
   }, [])
 
-  return !!isMobile
+  return isMobile
 }
