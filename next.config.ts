105e2e2692e97ae0b77f34d35df15e742cd61c5f
Pardi Pardi <pardi.jhr@gmail.com>
2025-12-15 02:33:46 +0000

    ⨯ ERROR: This build is using Turbopack, with a `webpack` config and no `

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type {NextConfig} from 'next';
+
 const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
+  // Other PWA options if needed
 });
 
 const nextConfig: NextConfig = {
